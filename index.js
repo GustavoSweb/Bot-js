79f67cfbc83cec985d6b5a45e5b563cc43c4cac9
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-10-31 09:59:45 -0300

    v5

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const Discord = require("discord.js")
+const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
   }
-});
+})
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
